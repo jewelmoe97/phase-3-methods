8d641384e9047ebac28552bb82c310e747094d72
jewel <snowdecember079@gmail.com>
2022-10-27 00:27:29 -0400

    commit tree 0c55e9ce0317132e25ee53670c49e2d70c56fc43

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet "Jewel"
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+greet_with_default "Jewel"
+
+def add(num1,num2)
+    return num1 + num2
+end
+sum1 = add(2, 2)
+
+def halve(num1)
+    if num1.class != Integer
+        return nil
+      end
+    
+    return num1/2
+    end
+    puts halve (10)
